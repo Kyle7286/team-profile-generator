4c0951d11fee2a58d300d1579be73087d6c2cdcb
Brandon Haley <kyle7286@gmail.com>
2021-02-23 14:50:00 -0500

    fully completed all constructures and sub classes with their methods and logic

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const team = new Team();
 const brandon = new Manager("Brandon", "1", "brandon@domain.com", "577");
 const kyle = new Engineer("Kyle", "2", "Kyle@domain.com", "www.kharris.github.com");
 const katie = new Engineer("Katie", "3", "Katie@domain.com", "www.ko.github.com");
-const paul = new Intern("Paul", "4", "Paul@domain.com", "www.ps.github.com");
-const david = new Intern("David", "5", "David@domain.com", "www.ds.github.com");
+const paul = new Intern("Paul", "4", "Paul@domain.com", "ga tech");
+const david = new Intern("David", "5", "David@domain.com", "ga state");
 
 team.addEmployee(brandon);
 team.addEmployee(kyle);
