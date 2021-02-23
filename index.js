fd2ba0c28bf0c112f1df18264210ee106680e490
Brandon Haley <kyle7286@gmail.com>
2021-02-23 17:22:14 -0500

    Completed setting up prompt functions for manager, intern, engineer and selections

--- index.js
+++ index.js
@@ -1,21 +1,152 @@
+// libraries
+const fs = require('fs');
+const util = require('util');
+const inquirer = require('inquirer');
+// convert writeFile (from FS) to async
+const writeFileAsync = util.promisify(fs.writeFile);
+// Classes
 const Team = require("./lib/Team");
 const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
-const team = new Team();
-const brandon = new Manager("Brandon", "1", "brandon@domain.com", "577");
-const kyle = new Engineer("Kyle", "2", "Kyle@domain.com", "www.kharris.github.com");
-const katie = new Engineer("Katie", "3", "Katie@domain.com", "www.ko.github.com");
-const paul = new Intern("Paul", "4", "Paul@domain.com", "ga tech");
-const david = new Intern("David", "5", "David@domain.com", "ga state");
 
-team.addEmployee(brandon);
-team.addEmployee(kyle);
-team.addEmployee(katie);
-team.addEmployee(paul);
-team.addEmployee(david);
 
-team.printTeam();
-console.log(team);
+
+// prompt, Main Menu
+const promptMainMenu = () => {
+    return inquirer.prompt([
+        {
+            type: 'confirm',
+            choices: ["Engineer", "Intern", "Finish"],
+            message: 'Please select which role you\'d like to add:',
+            name: 'selection',
+        },
+    ]);
+}
+
+// prompt, Selections
+const promptUserSelections = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            choices: ["Engineer", "Intern", "Finish"],
+            message: 'Please select which role you\'d like to add:',
+            name: 'selection',
+        },
+    ]);
+}
+
+
+
+// prompt, Manager
+const promptUserManager = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Name of Manager:',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Employee ID:',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'Email:',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'Office Number:',
+            name: 'office',
+        },
+    ]);
+}
+
+// prompt, Engineer
+const promptUserEngineer = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Name of Engineer:',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Employee ID:',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'Email:',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'Github:',
+            name: 'github',
+        },
+    ]);
+}
+
+// prompt, Intern
+const promptUserIntern = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Name of Engineer:',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Employee ID:',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'Email:',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'School:',
+            name: 'school',
+        },
+    ]);
+}
+
+// Do something with the prompts 
+const addManager = ({
+    name,
+    id,
+    email,
+    office
+}) => {
+
+
+    console.log(`${name} | ${id} | ${email}, ${office}`) //something here
+}
+
+const init = () => {
+    console.log(`Hello! Thank you for using Team Profile Generator!  Hope you find it to be very useful.
+    Please begin by entering the Manager's information...`);
+
+    promptUserManager()
+        .then(response => {
+
+            console.log(response);
+            addManager(response);
+
+            // writeFileAsync("./generated/README.md", readme)
+            //     .then(() => console.log("Success"))
+            //     .catch(err => console.log(err));
+        });
+}
+
+select
+
+// Run script on call
+init();
