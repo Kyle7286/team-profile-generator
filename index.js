afa6f58d0d6f19752c67e582a4c10866596c92be
Brandon Haley <kyle7286@gmail.com>
2021-02-23 11:57:32 -0500

    completed Team, Employee, Manager, engineer and Intern constructors

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const Team = require("./lib/Team");
 const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 const team = new Team();
-const brandon = new Employee("Brandon", "1", "brandon@domain.com");
+const brandon = new Manager("Brandon", "1", "brandon@domain.com", "577");
+const kyle = new Engineer("Kyle", "2", "Kyle@domain.com", "www.kharris.github.com");
+const katie = new Engineer("Katie", "3", "Katie@domain.com", "www.ko.github.com");
+const paul = new Intern("Paul", "4", "Paul@domain.com", "www.ps.github.com");
+const david = new Intern("David", "5", "David@domain.com", "www.ds.github.com");
+
 team.addEmployee(brandon);
-const kyle = new Employee("Kyle", "2", "Kyle@domain.com");
 team.addEmployee(kyle);
-
-
+team.addEmployee(katie);
+team.addEmployee(paul);
+team.addEmployee(david);
 
 team.printTeam();
