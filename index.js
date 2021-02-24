72ea5780ec3e464941a8ebca13a35852c2cbeb53
Brandon Haley <kyle7286@gmail.com>
2021-02-24 14:42:03 -0500

    refactored the Team object to seperate each role into their own arrays to loop thru later so we generate HTML in order from Manager -> Engineer -> Intern

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const util = require('util');
 const inquirer = require('inquirer');
+const chalk = require('chalk');
 // convert writeFile (from FS) to async
 const writeFileAsync = util.promisify(fs.writeFile);
 // Classes
@@ -34,13 +35,12 @@ const promptUserSelections = () => {
         },
     ]);
 }
-
 // prompt, Manager
 const promptUserManager = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            message: 'Name of Manager:',
+            message: 'Name:',
             name: 'name',
         },
         {
@@ -65,7 +65,7 @@ const promptUserEngineer = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            message: 'Name of Engineer:',
+            message: 'Name:',
             name: 'name',
         },
         {
@@ -90,7 +90,7 @@ const promptUserIntern = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            message: 'Name of Engineer:',
+            message: 'Name:',
             name: 'name',
         },
         {
@@ -112,24 +112,29 @@ const promptUserIntern = () => {
 }
 
 const init = () => {
-    console.log('\x1b[36m%s\x1b[0m',`
-    Hello! Thank you for using Team Profile Generator!  Hope you find it to be very useful.
+    console.log(chalk.bold.blue('Team Profile Generator'));
+    console.log(`
+    Hello! 
+    
+    `+ chalk.underline('Thank you') + ` SO much for using ` + chalk.bold.blue('Team Profile Generator') + `! Hope you find it to be very useful.
     Please begin by entering the Manager's information...
-    `);
+        `);
     promptManager();
 }
 
-
 function promptSelections() {
+    console.log("");
     promptUserSelections()
         .then(response => {
-            console.log("Selected something...");
+            console.log("");
             if (response.selection === "Engineer") { promptEngineer(); }
             if (response.selection === "Intern") { promptIntern(); }
             if (response.selection === "Finish") { promptFinish(); }
+            
         });
 }
 function promptManager() {
+    console.log(chalk.bold.greenBright("Manager"));
     promptUserManager()
         .then(response => {
             let manager = new Manager(response.name, response.id, response.email, response.office);
@@ -138,6 +143,7 @@ function promptManager() {
         });
 }
 function promptEngineer() {
+    console.log(chalk.bold.greenBright("Engineer"));
     promptUserEngineer()
         .then(response => {
             let engineer = new Engineer(response.name, response.id, response.email, response.github);
@@ -146,6 +152,7 @@ function promptEngineer() {
         });
 }
 function promptIntern() {
+    console.log(chalk.bold.greenBright("Intern"));
     promptUserIntern()
         .then(response => {
             let intern = new Intern(response.name, response.id, response.email, response.school);
@@ -155,7 +162,6 @@ function promptIntern() {
 }
 function promptFinish() {
     console.log(`All Done! Generating HTML...`);
-    console.log(team.printTeam());
     console.log(team);
 }
 function addToTeam(employee) {
