844e1446d91a1ebf6b934eb6e82cf29a0d980dd8
Brandon Haley <kyle7286@gmail.com>
2021-02-24 22:36:20 -0500

    Completed Card string logic; dynamic

--- index.js
+++ index.js
@@ -11,13 +11,12 @@ const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const buildHTML = require("./lib/buildHTML");
 // Global Declarations
-let team;
+let team; //be what a team be let
 
 
 // prompt, Main Menu
-const promptUserMainMenu = () => {
+const promptUserTeam = () => {
     return inquirer.prompt([
         {
             type: 'input',
@@ -112,14 +111,16 @@ const promptUserIntern = () => {
         },
     ]);
 }
-function promptMainMenu() {
+// Main Menu logic
+function promptTeam() {
     console.log(chalk.bold.greenBright("Team Name"));
-    promptUserMainMenu()
+    promptUserTeam()
         .then(response => {
             team = new Team(response.team);
             promptManager()
         });
 }
+// Selections Logic
 function promptSelections() {
     console.log("");
     promptUserSelections()
@@ -131,6 +132,7 @@ function promptSelections() {
 
         });
 }
+// Manager logic
 function promptManager() {
     console.log(chalk.bold.greenBright("Manager"));
     promptUserManager()
@@ -140,6 +142,7 @@ function promptManager() {
             promptSelections();
         });
 }
+// Engineer logic
 function promptEngineer() {
     console.log(chalk.bold.greenBright("Engineer"));
     promptUserEngineer()
@@ -149,6 +152,7 @@ function promptEngineer() {
             promptSelections();
         });
 }
+// Intern logic
 function promptIntern() {
     console.log(chalk.bold.greenBright("Intern"));
     promptUserIntern()
@@ -158,23 +162,91 @@ function promptIntern() {
             promptSelections();
         });
 }
+// Employee logic
 function addToTeam(employee) {
     team.addEmployee(employee);
 }
+// Finish logic
 function promptFinish() {
     console.log(`All Done! Generating HTML...`);
-    console.log(team);
+    // Build the HTML Strings
+    buildCardString(team);
 }
+// Build HTML string for all employee cards
+function buildCardString(obj) {
+    // console.log(obj.teamName);
+    // console.log(...obj.manager);
+    // console.log(...obj.engineer);
+    // console.log(...obj.intern);
+    let string = "";
+    let aObjPropertyValues = Object.values(obj)
+    console.log(aObjPropertyValues[3]);
+
+    // Loop thru items 1 - 3 of object property value array and build the all card string dynamically
+    for (let i = 1; i < 4; i++) {
+
+        let aRole = Object.values(obj)[i];
+        // console.log(aRole);
+
+
+
+
+        for (let j = 0; j < aRole.length; j++) {
+
+            let employee = aRole[j];
+            console.log(employee.name);
+            console.log(employee.role);
+
+            if (employee.role != "undefined") {
+
+                element = Object.values(obj)[i][j];
+                let sUniqueHTML;
+                if (element.officeNumber) { sUniqueHTML = `<p class="card-body-tile">Office: ${element.officeNumber}</p>` }
+                if (element.github) { sUniqueHTML = `<p class="card-body-tile">Github: ${element.github}</p>` }
+                if (element.school) { sUniqueHTML = `<p class="card-body-tile">School: ${element.school}</p>` }
+                string += `
+            
+            <div class="card ms-3 mt-3">
+    
+                        <div class="card-heading">
+                            <div class="heading-content m-2">
+                                <h5>${element.name}</h5>
+                                    <h6><i class="fas fa-mug-hot"></i> ${element.role}</h4>
+                            </div>
+                        </div>
+    
+                        <div class="card-body">
+                            <div class="body-content">
+                                <p class="card-body-tile">ID: ${element.id}</p>
+                                <p class="card-body-tile">Email: <a href="mailto: ${element.email}">${element.email}</a></p>
+                                ${sUniqueHTML}
+                            </div>
+                        </div>
+                    </div>
+    
+            `
+            }
+
+        }
+
+    }
+    console.log(`Current string= ${string}`);
+
+
+
+
+}
+
 
 const init = () => {
     console.log(chalk.bold.blue('Team Profile Generator'));
     console.log(`
-    Hello! 
-    
-    `+ chalk.underline('Thank you') + ` SO much for using ` + chalk.bold.blue('Team Profile Generator') + `! Hope you find it to be very useful.
+    Hello!
+
+            `+ chalk.underline('Thank you') + ` SO much for using` + chalk.bold.blue('Team Profile Generator') + `! Hope you find it to be very useful.
     Please begin by entering the Team's name! :)
-    `);
-    promptMainMenu();
+        `);
+    promptTeam();
 }
 
 // Run script on call
